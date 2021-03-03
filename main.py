4a3557aec9ddd0062426c5477dd2784720cbbb2b
SofiCibello <72713704+SofiCibello@users.noreply.github.com>
2021-03-03 12:34:40 -0300

    Usuarios cambiados

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class Servidor():
     # un while infinito que estará recorriendo la lista de clientes
     # para saber cuando recibe un mensaje.
     def procesarConexion(self):
-        usuarios = ['sofi,123', 'lara,123']
+        usuarios = ['Sofi,79906', 'Lara Parrucci,77749', 'Mario Groppo,1234']
         while True:
             if len(self.clientes) > 0:
                 clientesRemover = []
